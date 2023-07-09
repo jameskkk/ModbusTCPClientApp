6c2095012359e99ec392d3ab9d274c7988382226
jameskkk <chenjhkk@gmail.com>
2023-07-09 17:31:51 +0800

    Init first commit.

---
+++ ModbusTCPClientApp/FormMain.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace ModbusTCPClientApp
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRead = new System.Windows.Forms.Button();
+            this.txtIPAddress = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResultHex = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtResultDec = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtRegAddress = new System.Windows.Forms.TextBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(487, 17);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(121, 34);
+            this.btnRead.TabIndex = 0;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // txtIPAddress
+            // 
+            this.txtIPAddress.Location = new System.Drawing.Point(53, 25);
+            this.txtIPAddress.Name = "txtIPAddress";
+            this.txtIPAddress.Size = new System.Drawing.Size(224, 22);
+            this.txtIPAddress.TabIndex = 1;
+            this.txtIPAddress.Text = "127.0.0.1";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(357, 24);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(124, 22);
+            this.txtPort.TabIndex = 2;
+            this.txtPort.Text = "502";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(303, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Port:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Result HEX:";
+            // 
+            // txtResultHex
+            // 
+            this.txtResultHex.Location = new System.Drawing.Point(113, 100);
+            this.txtResultHex.Name = "txtResultHex";
+            this.txtResultHex.Size = new System.Drawing.Size(125, 22);
+            this.txtResultHex.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(247, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Result DEC:";
+            // 
+            // txtResultDec
+            // 
+            this.txtResultDec.Location = new System.Drawing.Point(342, 100);
+            this.txtResultDec.Name = "txtResultDec";
+            this.txtResultDec.Size = new System.Drawing.Size(139, 22);
+            this.txtResultDec.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Reg Address:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(247, 67);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Quantity:";
+            // 
+            // txtRegAddress
+            // 
+            this.txtRegAddress.Location = new System.Drawing.Point(113, 67);
+            this.txtRegAddress.Name = "txtRegAddress";
+            this.txtRegAddress.Size = new System.Drawing.Size(125, 22);
+            this.txtRegAddress.TabIndex = 11;
+            this.txtRegAddress.Text = "0";
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(342, 64);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(139, 22);
+            this.txtQuantity.TabIndex = 12;
+            this.txtQuantity.Text = "1";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 295);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.txtRegAddress);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtResultDec);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtResultHex);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIPAddress);
+            this.Controls.Add(this.btnRead);
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "ModbusTCP App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.TextBox txtIPAddress;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResultHex;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtResultDec;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtRegAddress;
+        private System.Windows.Forms.TextBox txtQuantity;
+    }
+}
+
---
+++ ModbusTCPClientApp/FormMain.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace ModbusTCPClientApp
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            //var request = new byte[]
+            //{
+            //          0x00, 0x01, // Transaction Identifier、連續數字或隨機數字都可以
+            //          0x00, 0x00, // Protocol Identifier、固定 0
+            //          0x00, 0x06, // Length、接下來有 6 bytes 的資料
+            //          0x01,       // Unit Identifier
+            //          0x03,       // Function Code
+            //          0x07, 0xD2, // Starting Address (2002 轉成 2 bytes 的結果)
+            //          0x00, 0x01, // Quantity of Registers (1 轉成 2 bytes 的結果)
+            //};
+            short address = short.Parse(txtRegAddress.Text);
+            short quantity = short.Parse(txtQuantity.Text);
+            var startingAddress = BitConverter.GetBytes(address);
+            var quantityOfRegisters = BitConverter.GetBytes(quantity);
+            // TCP 傳送都是以 Big Endian
+            if (BitConverter.IsLittleEndian)
+            {
+                startingAddress = startingAddress.Reverse().ToArray();
+                quantityOfRegisters = quantityOfRegisters.Reverse().ToArray();
+            }
+            var request = new byte[]
+            {
+              0x00, 0x01, // Transaction Identifier、連續數字或隨機數字都可以
+              0x00, 0x00, // Protocol Identifier、固定 0
+              0x00, 0x06, // Length、接下來有 6 bytes 的資料
+              0x01,       // Unit Identifier
+              0x03,       // Function Code
+              startingAddress[0], startingAddress[1],
+              quantityOfRegisters[0], quantityOfRegisters[1] // Quantity of Registers (1 轉成 2 bytes 的結果)
+            };
+
+            // 建立 TcpClient 並進行連線
+            var tcpClient = new TcpClient();
+            tcpClient.Connect(txtIPAddress.Text, int.Parse(txtPort.Text));
+
+            NetworkStream stream = tcpClient.GetStream();
+            stream.Write(request, 0, request.Length);
+
+            var response = new byte[1024];
+            var readCount = stream.Read(response, 0, response.Length);
+            Console.WriteLine("readCount = " + readCount.ToString());
+            // 比較完整的寫法是把 Transaction Identifier、Function Code 等都抓出來檢查
+            // 尤其是 Function Code，因為當有 error 時就不會是 0x03，而是 0x83 了
+            // 不過這邊就先省略，以抓到我們的目標資料為主，那麼就是最後的 quantity * 2 個 bytes
+            //var result = response.Take(readCount).TakeLast(quantity * 2).ToArray();
+            var result = response.Skip(readCount - (quantity * 2)).Take(quantity * 2).ToArray();
+            string hexString = BitConverter.ToString(result).Replace("-", "");
+            Console.WriteLine("result = " + hexString);
+            txtResultHex.Text = hexString;
+            txtResultDec.Text = Convert.ToInt32(hexString, 16).ToString();
+        }
+    }
+}
